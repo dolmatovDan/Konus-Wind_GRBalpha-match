84c80a396269263f86e039dfb1c52a2c7fbd329b
dolmatovDan <58562604+dolmatovDan@users.noreply.github.com>
2024-04-23 20:46:19 +0300

    parse GRBalpha data

---
+++ src/data/parse_GRBalpha_data.py
@@ -0,0 +1,56 @@
+import sys
+import requests
+from bs4 import BeautifulSoup as bs
+
+sys.path.append("..")
+from utility import create_folder, clear_name
+
+baseurl = "https://monoceros.physics.muni.cz/hea/GRBAlpha/"
+r = requests.get(baseurl)
+soup = bs(r.content)
+
+# len(soup.find_all("table")) = 1
+# This means, that the first one in necessary data
+table = soup.find("table")
+
+headers = [
+    "Event type/name",
+    "Peak time (UTC)",
+    "T90 [s]",
+    "Peak count rate [cnt/s]",
+    "Band [keV]",
+    "S/N [sigma]",
+    "Raw LC",
+    "Bkg-sub LC",
+    "LC res. [s]",
+    "GCN circ.",
+    "References",
+    "Comment",
+]
+
+for index, row in enumerate(table.find_all("tr")):
+    if index == 0:  # Headers
+        continue
+    columns = row.select("td")
+
+    event_name = columns[0].string
+    peak_time = columns[1].string
+
+    event_type = event_name.split()[0]
+    data_folder = "../../data/raw/"
+    current_folder = f"{data_folder}{clear_name(event_name)}_{index:02d}/"
+
+    create_folder(current_folder)
+
+    with open(f"{current_folder}info.txt", "w") as info_file:
+        info_columns = [0, 1, 2, 3, 4, 5]
+        for column in info_columns:
+            print(f"({headers[column]}): {columns[column].string}", file=info_file)
+
+    for link in row.find_all("a"):
+        full_url = baseurl + str(link.get("href"))
+        if full_url.endswith(".txt"):
+            print(full_url)
+            link = requests.get(full_url)
+            with open(f"{current_folder}data.txt", "w") as save_file:
+                save_file.write(str(link.content))
--- src/utility/methods.py
+++ src/utility/methods.py
@@ -7,3 +7,30 @@ def create_folder(name):
     else:
         path = name
         os.mkdir(path)
+
+
+def get_folder_size(folder_name):
+    return len(
+        [
+            name
+            for name in os.listdir(folder_name)
+            if os.path.isfile(os.path.join(folder_name, name))
+        ]
+    )
+
+
+def get_folders_num(folder_name):
+    return len(
+        [
+            name
+            for name in os.listdir(folder_name)
+            if os.path.isdir(os.path.join(folder_name, name))
+        ]
+    )
+
+
+def clear_name(name):
+    if name.find("/") == -1:
+        return name
+    pos = name.find("/")
+    return name[:pos]
